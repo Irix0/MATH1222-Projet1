8efbfa17e59f5f1954c68b763343c9a9713ef78a
erivdv <erivdv@hotmail.com>
2023-04-17 22:47:12 +0200

    Changements mineures

--- part2/decrypt.py
+++ part2/decrypt.py
@@ -4,32 +4,34 @@ import random
 # Reads a file that contains all possible symbols and creates a list of them
 def symbols_reading(filename):
     symbols = []
-    
-    file = open("characters.txt","r")
+
+    file = open(filename,"r")
     symb = file.read(1)
     while symb:
-        if symb != '\n':
+        if symb != '\n' and symb not in symbols:
             symbols.append(symb)
         symb = file.read(1)
     file.close()
     return symbols
 
-# Create a dictionnary for the character counter, a dictionnary for theta and a dictrionnary for the transitions between symbols
+# Create a dictionnary for the symbol counter, a dictionnary for theta and a dictrionnary for the transitions between symbols
 def create_dictionnaries(symbols):
-    char_count = {}
+    symb_count = {}
     theta = {}
     transition_count = {}
 
     for i in symbols:
-        char_count[i] = 0
+        symb_count[i] = 0
         theta[i] = i
-        transition_count = {}
+        transition_count[i] = {}
         for j in symbols:
             transition_count[i][j] = 0
-    return char_count, theta, transition_count
+    return symb_count, theta, transition_count
 
-def probabilities(filename):
+def probabilities(filename, char_count, transition_count):
+    total_char = 0
 
+    # Count the number of each symbols and their transitions by reading filename
     file = open(filename, "r")
     curr_char = file.read(1)
     next_char = file.read(1)
@@ -38,58 +40,74 @@ def probabilities(filename):
             char_count[curr_char] += 1
             if next_char in transition_count[curr_char]:
                transition_count[curr_char][next_char] += 1
+            total_char += 1
         curr_char, next_char = next_char, file.read(1)
+    file.close()
 
-    # Calculate the apparition probabilities
+    # Calculate the apparition probabilities and the transition probabilities
     apparition_probabilities = {}
-    total_char = 0
-    for curr_char in char_count:
-        total_char += char_count[curr_char]
-    for curr_char in char_count:
-        apparition_probabilities[curr_char] = char_count[curr_char] / total_char
-
-    # Calculate the transition probabilities
     transition_probabilities = {}
     for curr_char in transition_count:
+        apparition_probabilities[curr_char] = char_count[curr_char] / total_char
         transition_probabilities[curr_char] = {}
         for next_char in transition_count[curr_char]:
             transition_probabilities[curr_char][next_char] = transition_count[curr_char][next_char] / char_count[curr_char]
 
-    file.close()
     return apparition_probabilities, transition_probabilities
 
+# Create a new substitution by swapping key1 and key2
+def create_new_theta(key1, key2, theta):
+    new_theta = theta.copy()
+    new_theta[key2] = theta[key1]
+    new_theta[key1] = theta[key2]
+    return new_theta
 
-def swap_theta(key1, key2, theta):
-    tmp = theta[key1]
-    theta[key1] = theta[key2]
-    theta[key2] = tmp
-    return theta
-
-def calculate_likelihood(string, u, P):
-    likelihood = math.log(u[string[0]])
-
+# Calculate the likelihood of a string
+def calculate_likelihood(string, app_prob, trans_prob):
+    likelihood = math.log(app_prob[string[0]])
     for i in range(len(string)-1):
         try :
-            likelihood += math.log(P[string[i]][string[i+1]])
+            likelihood += math.log(trans_prob[string[i]][string[i+1]])
         except:
-            likelihood = float('-inf')
+            likelihood += -100
     return likelihood
 
+# Create a new string from another by using a substitution code
 def decrypt(string, theta):
     new_string = ""
     for i in range(len(string)):
         new_string += theta[string[i]]
     return new_string
 
-def metropolis_hastings(string, theta, u, P):
+# Determine the substitution code that has the best likelihood
+# Does not work
+def metropolis_hastings(string, theta, app_prob, trans_prob, nb_iter):
+    likelihood = calculate_likelihood(string, app_prob, trans_prob)
+
+    i = 0
+    while i < nb_iter:
+        key1, key2 = random.sample(list(theta), 2)
+        new_theta = create_new_theta(key1, key2, theta)
+
+        new_string = decrypt(string, theta)
+        new_likelihood = calculate_likelihood(new_string, app_prob, trans_prob)
+
+        alpha = min(1, math.exp(new_likelihood - likelihood))
+        if random.random() < alpha:
+            theta = new_theta
+            likelihood = new_likelihood
+        print(likelihood)
+        i += 1
     return theta
 
-symbols = symbols_reading("characters.txt")
+string1 = "wlenhenhjhnlm,whwznwhwz" + '"' + "whwmhdzhzsk,t;wz):"
+print(string1)
 
-string1 = "wlenhenhjhnlm,whwznwhwzwhwmhdzhzsk,t;wz):"
+symbols = symbols_reading("symbols.txt")
+symb_count, theta, transition_count = create_dictionnaries(symbols)
+app_prob, trans_prob = probabilities("moby_dick.txt", symb_count, transition_count)
 
-char_count, theta = characters_reading("characters.txt")
-u, P = transition_probabilities("moby_dick.txt", char_count)
-
-theta = metropolis_hastings(string1, theta, u, P)
+theta = metropolis_hastings(string1, theta, app_prob, trans_prob, 10000)
 new_string = decrypt(string1, theta)
+
+print(new_string)
